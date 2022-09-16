45dcf4ed242c53c31e437fbc57f6af32a42fa8db
yummyhotpot <a25580819a1107@gmail.com>
2022-09-16 18:48:50 +0800

    視窗程式作業ex1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,45 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("hello");
+            int num;
+            bool succes = int.TryParse(textbox.Text, out num);
+            if (!succes)
+            {
+                MessageBox.Show("輸入錯誤，請輸入正整數");
+            }
+            else
+            {
+                int a = 0;
+                string ans = $"小於等於{num}的質數為:";
+                string aa = "";
+                ans += "2";
+                for (int i = 3; i <= num; i++)
+                {
+                    a = 0;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            a++;
+                        }
+                    }
+                    if (a == 2)
+                    {
+                        ans += ", ";
+                        ans += $"{i}";
+                        aa += $"\n{i}的倍數:";
+                        for (int k = 1; k <= (num / i); k++)
+                        {
+                            aa += $"{i * k}";
+                            if (k != (num / i)) { aa += ", "; }
+                            else { aa += "。"; }
+                        }
+                    }
+                }
+                ans += "。";
+                textbox2.Text = $"{ans}{aa}";
+
+            }
         }
     }
 }
